e90886b339e096b783b596c7775bc3085f3cb8de
liuding <liuding@ifmuse.com>
2017-03-07 20:23:13 +0800

    add writeFile.js

--- file-api/readFile.js
+++ file-api/readFile.js
@@ -1,12 +1,17 @@
 
+/*
+* node.JS offers file reading API through Node.JS's 'fs' module.
+* sync read API : fs.readFileSync
+* async read API : fs.readFile
+*/
 var fs = require("fs");
 
-// 同步
+// sync
 var data = fs.readFileSync("./name.txt");
 console.log("同步读取完毕：\n" + data.toString() + "\n");
 
 
-// 异步
+// async
 fs.readFile("./name.txt" , function(err , data){
 	if(err){
 		return console.err(err);
---
+++ file-api/writeFile.js
@@ -0,0 +1,23 @@
+
+/*
+* Node.JS's file writing API through 'fs' module 
+* fs.writeFile(file , data [,options] , callback)
+*/
+
+var fs = require("fs") ;
+
+
+fs.readFile("./name.txt" , function(err , data){
+
+	if(err){
+		return console.err(err);
+	}
+	console.log("开始写入!\n");
+	fs.writeFile("./name.txt" , data.toString()+" new"  , function(err){
+		if(err){
+			return console.err(err);
+		}
+		console.log("写入成功！\n");
+	});
+
+});
